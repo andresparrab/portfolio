ad56a1be8feaecb1e6cdf77f10f66e619bc7f631
doctor <andres.parrab@gmail.com>
2020-12-03 11:50:33 +0100

    update4

--- my-portfolio/src/components/Skills/Skills.tsx
+++ my-portfolio/src/components/Skills/Skills.tsx
@@ -4,88 +4,100 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <div>
-      <div className='skills'>
+      <div className="skills">
         <h1>Skills</h1>
         <li>
           <h3>HTML5</h3>
-          <span className='bar'>
-            <span className='html'></span>{" "}
+          <span className="bar">
+            <span className="html"></span>{" "}
           </span>
         </li>
 
         <li>
           <h3>CSS</h3>
-          <span className='bar'>
+          <span className="bar">
             {" "}
-            <span className='css'></span>
+            <span className="css"></span>
           </span>
         </li>
 
         <li>
           <h3>Javascript</h3>
-          <span className='bar'>
-            <span className='javasc'></span>{" "}
+          <span className="bar">
+            <span className="javasc"></span>{" "}
           </span>
         </li>
 
         <li>
           <h3>React</h3>
-          <span className='bar'>
+          <span className="bar">
             {" "}
-            <span className='react'></span>
+            <span className="react"></span>
           </span>
         </li>
 
         <li>
           <h3>Express</h3>
-          <span className='bar'>
-            <span className='express'></span>{" "}
+          <span className="bar">
+            <span className="express"></span>{" "}
           </span>
         </li>
 
         <li>
           <h3>Mongodb</h3>
-          <span className='bar'>
-            <span className='express'></span>{" "}
+          <span className="bar">
+            <span className="express"></span>{" "}
           </span>
         </li>
 
         <li>
           <h3>Firebase</h3>
-          <span className='bar'>
-            <span className='express'></span>{" "}
+          <span className="bar">
+            <span className="express"></span>{" "}
           </span>
         </li>
 
         <li>
-          <h3>EXPRESS</h3>
-          <span className='bar'>
-            <span className='express'></span>{" "}
+          <h3>Python</h3>
+          <span className="bar">
+            <span className="python"></span>{" "}
+          </span>
+        </li>
+        <li>
+          <h3>SQL</h3>
+          <span className="bar">
+            <span className="sql"></span>{" "}
+          </span>
+        </li>
+        <li>
+          <h3>Docker</h3>
+          <span className="bar">
+            <span className="docker"></span>{" "}
           </span>
         </li>
       </div>
 
-      <div className='Languages'>
+      <div className="Languages">
         <h1>Languages</h1>
         <li>
           <h3>Spanish</h3>
-          <span className='bar'>
-            <span className='spanish'></span>{" "}
+          <span className="bar">
+            <span className="spanish"></span>{" "}
           </span>
         </li>
 
         <li>
           <h3>Swedish</h3>
-          <span className='bar'>
+          <span className="bar">
             {" "}
-            <span className='swedish'></span>
+            <span className="swedish"></span>
           </span>
         </li>
 
         <li>
           <h3>English</h3>
-          <span className='bar'>
-            <span className='english'></span>{" "}
+          <span className="bar">
+            <span className="english"></span>{" "}
           </span>
         </li>
       </div>
--- my-portfolio/src/views/aboutView.tsx
+++ my-portfolio/src/views/aboutView.tsx
@@ -6,18 +6,19 @@ import profile from "../images/profile02.jpg";
 //import { isImportOrExportSpecifier } from "typescript";
 const aboutView = () => {
   return (
-    <div className='content-wrapper'>
+    <div className="content-wrapper">
       {/*<!-- This is where everythin below the navbar is going to be-->*/}
-      <div className='two-column-wrapper'>
-        <div className='profile-image-wrapper'>
-          <img src={profile} alt='imgd' />
+      <div className="two-column-wrapper">
+        <div className="profile-image-wrapper">
+          <img src={profile} alt="imgd" />
         </div>
 
-        <div className='profile-content-wrapper'>
+        <div className="profile-content-wrapper">
           <h1>Hi, thanks for visiting!</h1>
           <p>
-            "Programming isn't about what you know; it's about what you can figure out.” - Chris Pine Self-taught software developer.
-            Passionate about everything in technology, programming, personal development and cooking.
+            "Programming isn't about what you know, it's about what you can figure out.” - Chris Pine <br />
+            Self-taught software developer. Passionate about everything in technology, programming, personal development
+            and cooking.
           </p>
           <div>
             <Skills></Skills>
